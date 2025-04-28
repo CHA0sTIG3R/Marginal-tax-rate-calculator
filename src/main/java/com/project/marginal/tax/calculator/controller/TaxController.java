2ba84b8fb5d97f24f093235ec44306274e015c98
Hamza <olowuhamzat@gmail.com>
2025-04-28 16:03:26 -0400

    Added copyright notices to multiple Java files

--- src/main/java/com/project/marginal/tax/calculator/controller/TaxController.java
+++ src/main/java/com/project/marginal/tax/calculator/controller/TaxController.java
@@ -1,9 +1,19 @@
+/*
+ * Copyright 2025 Hamzat Olowu
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * GitHub: https//github.com/CHA0sTIG3R
+ */
+
 package com.project.marginal.tax.calculator.controller;
 
 import com.project.marginal.tax.calculator.dto.*;
 import com.project.marginal.tax.calculator.entity.FilingStatus;
 import com.project.marginal.tax.calculator.service.TaxService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/project/marginal/tax/calculator/repository/TaxRateRepository.java
+++ src/main/java/com/project/marginal/tax/calculator/repository/TaxRateRepository.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2025 Hamzat Olowu
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * GitHub: https//github.com/CHA0sTIG3R
+ */
+
 package com.project.marginal.tax.calculator.repository;
 
 import com.project.marginal.tax.calculator.entity.FilingStatus;
