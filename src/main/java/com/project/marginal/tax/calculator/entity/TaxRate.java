dc351cd656caaca2829ee7af73370a4b5ee1bcc3
Hamza <olowuhamzat@gmail.com>
2025-06-15 09:51:28 -0400

    Enhance TaxRate entity with Lombok annotations for boilerplate reduction

--- src/main/java/com/project/marginal/tax/calculator/entity/TaxRate.java
+++ src/main/java/com/project/marginal/tax/calculator/entity/TaxRate.java
@@ -12,6 +12,9 @@
 package com.project.marginal.tax.calculator.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
@@ -21,6 +24,9 @@ import java.math.BigDecimal;
  * This entity is used to store historical tax rates in the database.
  * </p>
  */
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "historical_tax_rates")
 public class TaxRate {
@@ -36,51 +42,20 @@ public class TaxRate {
     private BigDecimal rangeStart;
     private BigDecimal rangeEnd;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
+    /**
+     * Constructs a new TaxRate with the specified parameters.
+     *
+     * @param year       the tax year
+     * @param status     the filing status
+     * @param rate       the tax rate as a percentage (e.g., 0.10 for 10%)
+     * @param rangeStart the start of the income range for this tax rate
+     * @param rangeEnd   the end of the income range for this tax rate
+     */
+    public TaxRate(Integer year, FilingStatus status, Float rate, BigDecimal rangeStart, BigDecimal rangeEnd) {
         this.year = year;
-    }
-
-    public FilingStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(FilingStatus status) {
         this.status = status;
-    }
-
-    public Float getRate() {
-        return rate;
-    }
-
-    public void setRate(Float rate) {
         this.rate = rate;
-    }
-
-    public BigDecimal getRangeStart() {
-        return rangeStart;
-    }
-
-    public void setRangeStart(BigDecimal rangeStart) {
         this.rangeStart = rangeStart;
-    }
-
-    public BigDecimal getRangeEnd() {
-        return rangeEnd;
-    }
-
-    public void setRangeEnd(BigDecimal rangeEnd) {
         this.rangeEnd = rangeEnd;
     }
 
